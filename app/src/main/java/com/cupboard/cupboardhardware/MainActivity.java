b8f654b887a5ea8ac367d32ca0693fcc1426d878
zhoumao <zhoumao@meituan.com>
2018-10-12 13:46:35 +0800

    添加串口jni及相关函数；
    Signed-off-by: zhoumao

--- app/src/main/java/com/cupboard/cupboardhardware/MainActivity.java
+++ app/src/main/java/com/cupboard/cupboardhardware/MainActivity.java
@@ -1,13 +1,30 @@
 package com.cupboard.cupboardhardware;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import com.cupboard.serial.SerialPortUtil;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    SerialPortUtil mSerialPortUtil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mSerialPortUtil = new SerialPortUtil();
+        initView();
+    }
+
+    private void initView() {
+        findViewById(R.id.btn_seriallist).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: " + mSerialPortUtil.getAllSerialPort());
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/cupboard/serial/SerialPortUtil.java
@@ -0,0 +1,128 @@
+package com.cupboard.serial;
+
+import android.util.Log;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * @author by AllenJ on 2018/4/20.
+ *
+ *         通过串口用于接收或发送数据
+ */
+
+public class SerialPortUtil {
+
+    private SerialPort serialPort = null;
+    private SerialPortFinder mSerialPortFinder;
+    private InputStream inputStream = null;
+    private OutputStream outputStream = null;
+    private ReceiveThread mReceiveThread = null;
+    private boolean isStart = false;
+
+    private static final String TAG = "SerialPortUtil";
+
+
+    public SerialPortUtil() {
+        mSerialPortFinder = new SerialPortFinder();
+    }
+
+
+    public String[] getAllSerialPort() {
+        return mSerialPortFinder.getAllDevices();
+    }
+
+    /**
+     * 打开串口，接收数据
+     * 通过串口，接收单片机发送来的数据
+     */
+    public void openSerialPort() {
+        try {
+            serialPort = new SerialPort(new File("/dev/ttyS0"), 9600, 0);
+            //调用对象SerialPort方法，获取串口中"读和写"的数据流
+            inputStream = serialPort.getInputStream();
+            outputStream = serialPort.getOutputStream();
+            isStart = true;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        getSerialPort();
+    }
+
+    /**
+     * 关闭串口
+     * 关闭串口中的输入输出流
+     */
+    public void closeSerialPort() {
+        Log.i("test", "关闭串口");
+        try {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+            if (outputStream != null) {
+                outputStream.close();
+            }
+            isStart = false;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * 发送数据
+     * 通过串口，发送数据到单片机
+     *
+     * @param data 要发送的数据
+     */
+    public void sendSerialPort(String data) {
+        try {
+            byte[] sendData = DataUtils.HexToByteArr(data);
+            outputStream.write(sendData);
+            outputStream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void getSerialPort() {
+        if (mReceiveThread == null) {
+
+            mReceiveThread = new ReceiveThread();
+        }
+        mReceiveThread.start();
+    }
+
+    /**
+     * 接收串口数据的线程
+     */
+
+    private class ReceiveThread extends Thread {
+        @Override
+        public void run() {
+            super.run();
+            //条件判断，只要条件为true，则一直执行这个线程
+            while (isStart) {
+                if (inputStream == null) {
+                    return;
+                }
+                byte[] readData = new byte[1024];
+                try {
+                    int size = inputStream.read(readData);
+                    if (size > 0) {
+                        String readString = DataUtils.ByteArrToHex(readData, 0, size);
+                        //TODO 发布数据
+                        Log.d(TAG, readString);
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+    }
+}
